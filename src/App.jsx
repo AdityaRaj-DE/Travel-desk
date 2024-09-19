012cfece3216ae04429ce451e719a6a25492d007
Apeksha <apekshas729@gmail.com>
2024-09-19 18:10:00 +0530

    Apeksha Shukla

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,9 @@ function App() {
 
   return (
     <>
-<<<<<<< HEAD
       <div>Aditya : hello</div>
     <div>Apeksha</div>
     ___
-=======
-    <div className='text'>
-      <div>Aditya Raj: hello</div>
-      <div>Shreya : hi</div>
-      <div>Shivam: hello</div>
-      <div>Shashi kant: hello</div>
-      <div>Shashi kant: Testing...</div>
-      <div>Shashi kant: Leave</div>
-      <div>Abhay : hi</div>
-      <div>vaibhav : hello</div>
-    </div>
-      
->>>>>>> 8813c63dd87bb579195b6f8822b02606354f5a2d
     </>
   )
 }
