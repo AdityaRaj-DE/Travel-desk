753fb068b9924d6a8cf05127fd5078fdd66e94ab
Aditya Raj <adityarajk2002@gmail.com>
2024-10-02 19:05:35 +0530

    fixtailwind

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 function Navbar() {
   return (
-    <div className='m-2 text-lime-500 bg-sky-500'>
+    <div className='m-2 text-lime-500'>
       <NavLink className='p-2 text-lime-500 text-3xl' to="/">TravelDesk</NavLink>
       <NavLink className='p-2 text-lime-500' to="/about">About</NavLink>
       <NavLink className='p-2 text-lime-500' to="/redeem">Redeem</NavLink>
