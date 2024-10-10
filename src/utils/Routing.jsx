8d0f3299cdab1a6cf92728758f81386bae06922d
Aditya Raj <adityarajk2002@gmail.com>
2024-10-10 17:46:42 +0530

    placePlaces

--- src/utils/Routing.jsx
+++ src/utils/Routing.jsx
@@ -5,6 +5,8 @@ import {Route, Routes} from "react-router-dom"
 import Redeem from '../components/Redeem'
 import Login from '../components/Login'
 import Signup from '../components/Signup'
+import Place from '../components/Place'
+import Places from '../components/Places'
 
 const Routing = () => {
   return (
@@ -15,6 +17,8 @@ const Routing = () => {
         <Route path="/redeem" element={<Redeem/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/signup" element={<Signup/>}/>
+        <Route path="/:places" element={<Places/>}/>
+        <Route path="/:place" element={<Place/>}/>
       </Routes>
     </div>
   )
